12cab6e9e9f9330a88238ad98a95a255f8311e01
ct-balt <evald.zuralovic@coraltravel.lt>
2025-08-29 09:57:10 +0300

    Add files via upload

---
+++ src/constants/departuresConstants.js
@@ -0,0 +1,8 @@
+const departuresConstants = [
+  {
+    id: "3345-5",
+    type: 5,
+    name: "Rīga",
+    friendlyUrl: "riga",
+  },
+];
---
+++ src/constants/destinationsConstants.js
@@ -0,0 +1,67 @@
+const destinationsConstants = [
+  {
+    country: "turcija",
+    name: "Turcija",
+    friendlyUrl: "turcija",
+    id: "1-0",
+    type: 0,
+    children: [
+      {
+        id: "5-3",
+        type: 3,
+        name: "Antālija",
+        friendlyUrl: "antalija",
+      },
+      {
+        id: "1-3",
+        type: 3,
+        name: "Bodruma",
+        friendlyUrl: "bodruma",
+      },
+    ],
+  },
+  {
+    country: "griekija",
+    name: "Grieķija",
+    friendlyUrl: "griekija",
+    id: "43-0",
+    type: 0,
+    children: [
+      {
+        id: "143-3",
+        type: 3,
+        name: "Heraklion",
+        friendlyUrl: "heraklion",
+      },
+    ],
+  },
+  {
+    country: "egipte",
+    name: "Ēģipte",
+    friendlyUrl: "egipte",
+    id: "12-0",
+    type: 0,
+    children: [
+      {
+        id: "70-3",
+        type: 3,
+        name: "Hurghada",
+        friendlyUrl: "hurghada",
+      },
+      {
+        id: "83-3",
+        type: 3,
+        name: "Sharm El Sheikh",
+        friendlyUrl: "sharmelsheikh",
+      },
+    ],
+  },
+  {
+    country: "apvienotiearabuemirati",
+    name: "Apvienotie Arābu Emirāti",
+    friendlyUrl: "apvienotiearabuemirati",
+    id: "31-0",
+    type: 0,
+    children: null,
+  },
+];
